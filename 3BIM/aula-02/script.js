7bdf3b08aaad803fab0581252e104b3cea70507a
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-09-11 17:45:09 -0300

    feat: add exercicio 5 aula 02 do 3 bimestre

--- 3BIM/aula-02/script.js
+++ 3BIM/aula-02/script.js
@@ -17,3 +17,32 @@ function calcularPreco() {
     var precoTotal = (quilo * consumo) / 1000;
     console.log(precoTotal);
 }
+
+// funções para exercício 5
+function limparCampos() {
+    // Limpa os conteúdos dos elementos
+    document.getElementById("inNome").value = "";
+    document.getElementById("rbMasculino").checked = false;
+    document.getElementById("rbFeminino").checked = false;
+    document.getElementById("inAltura").value = "";
+    // document.getElementById("outResposta").textContent = "";
+    // posiciona (joga o foco) no elemento inNome
+    document.getElementById("inNome").focus();
+    }
+
+function calcularPeso() {
+    var altura = document.getElementById("inAltura").value;
+
+    if (document.getElementById("rbMasculino").checked == true) {
+        var pesoIdeal = (altura * altura) * 22;
+        console.log(pesoIdeal/10000);
+    } else {
+        var pesoIdeal = (altura * altura) * 21;
+        console.log(pesoIdeal/10000);
+    }
+}    
+    var btLimpar = document.getElementById("btLimpar");
+    btLimpar.addEventListener("click", limparCampos);
+
+    var btCalcular = document.getElementById("btCalcular");
+    btCalcular.addEventListener("click", calcularPeso);
